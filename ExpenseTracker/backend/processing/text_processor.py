82ac105fff1ba1d823800422cca9e2ed8a6803a7
toeneeoh <toeneeoh@gmail.com>
2024-10-30 12:56:55 -0400

    Fixing backend

--- ExpenseTracker/backend/processing/text_processor.py
+++ ExpenseTracker/backend/processing/text_processor.py
@@ -2,8 +2,11 @@ from flask import Blueprint, request, jsonify
 
 bp = Blueprint('text', __name__, url_prefix='/text')
 
-@bp.route('/process', methods=['POST'])
+@bp.route('/process', methods=['POST', 'OPTIONS'])
 def process_text():
+    if request.method == 'OPTIONS':
+            return '', 200  # Respond to preflight request
+
     data = request.get_json()
     text = data.get('text', '')
     processed = text.upper()
