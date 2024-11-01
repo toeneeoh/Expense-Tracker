bc96abe96439c5627496ff43bdea7c28feb63b78
Zach Hamilton <hamiltonzacharya@gmail.com>
2024-11-01 15:27:40 -0400

    Front End Patches
    Various fixes for Front End problems and cleaning up code, part of our progress fixing various bugs that have appeared

--- ExpenseTracker/app/(tabs)/notifications.tsx
+++ ExpenseTracker/app/(tabs)/notifications.tsx
@@ -4,8 +4,8 @@ import { StyleSheet, Image, Platform } from 'react-native';
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import ThemedText from '@/components/ThemedText';
+import ThemedView from '@/components/ThemedView';
 
 export default function TabTwoScreen() {
   return (
