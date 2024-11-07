3f7c4950b521fd2a8b00f5dc5b924f1eb5ca20c2
singale-1 <singale@gmu.edu>
2024-11-07 11:18:14 -0500

    Update tst.py

--- ExpenseTracker/backend/tst.py
+++ ExpenseTracker/backend/tst.py
@@ -1,4 +1,10 @@
-Traceback (most recent call last):
+import sys
+import unittest
+
+unittest.main(module=None, argv=['', 'discover', '-s', 'tests'])
+sys.path.append(".")
+
+# Traceback (most recent call last):
   File "<frozen runpy>", line 198, in _run_module_as_main
   File "<frozen runpy>", line 88, in _run_code
   File "C:\Users\singale-admin\AppData\Local\Programs\Python\Python313\Lib\unittest\__main__.py", line 18, in <module>
@@ -18,4 +24,4 @@ Traceback (most recent call last):
                 ~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   File "C:\Users\singale-admin\AppData\Local\Programs\Python\Python313\Lib\unittest\loader.py", line 308, in discover
     raise ImportError('Start directory is not importable: %r' % start_dir)
-ImportError: Start directory is not importable: 'tests'
+# ImportError: Start directory is not importable: 'tests'
