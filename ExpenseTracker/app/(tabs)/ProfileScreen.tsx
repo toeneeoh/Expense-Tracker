6e68f26631d8be45ef7543425f9539a468f8c353
toeneeoh <toeneeoh@gmail.com>
2024-11-20 17:17:16 -0500

    hard code colors

--- ExpenseTracker/app/(tabs)/ProfileScreen.tsx
+++ ExpenseTracker/app/(tabs)/ProfileScreen.tsx
@@ -1,16 +1,16 @@
 import { ThemedView, ThemedText } from '@/components/ThemedComponents';
 import DropdownField from '@/components/DropdownField';
 import InputField from '@/components/InputField';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 
 const cities = ['Washington, D.C.', 'Fairfax', 'Arlington', 'Austin', 'Sacramento', 'Denver']; // Get more major cities
 
 export default function ProfileScreen() {
   return (
-    <ThemedView style={styles.container}>
+    <View style={styles.container}>
 
-      <ThemedText type="title">Location Da</ThemedText>
+      <ThemedText type="title" style={styles.title}>Location Data</ThemedText>
 
           <DropdownField label="What city do you live in?" options={cities} />
           <DropdownField label="What state do you live in?" options={cities} />
@@ -20,12 +20,15 @@ export default function ProfileScreen() {
           <InputField label="How many bedrooms do you need in your home?" />
       {/*<InputField label="Any certifications / professional skills" />*/}
 
-    </ThemedView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     margin: 16,
+  },
+  title: {
+    color: '#000000',
   },
 });
