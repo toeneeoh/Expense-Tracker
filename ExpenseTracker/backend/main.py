2027a8ccf16446e8d6c098919ebdd19ce604221c
toeneeoh <toeneeoh@gmail.com>
2024-12-21 13:35:33 -0500

    automate ngrok tunnel

--- ExpenseTracker/backend/main.py
+++ ExpenseTracker/backend/main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 import routes
 from database.postgres import init_db
+from pyngrok import ngrok
+from config import NGROK_DOMAIN
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -13,4 +15,9 @@ if __name__ == '__main__':
     # initialize PostgreSQL database
     init_db()
 
-    app.run(host='0.0.0.0', debug=True)
+    # start Ngrok tunnel
+    ngrok_tunnel = ngrok.connect(5000, bind_tls=True, hostname=NGROK_DOMAIN)
+    print(f" * Ngrok tunnel started at: {ngrok_tunnel.public_url}")
+    app.config['SERVER_NAME'] = NGROK_DOMAIN
+
+    app.run(host='0.0.0.0', debug=False) # ngrok fails with True due to rerunning the script
